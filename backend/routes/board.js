693f7b61a2de4a0bdbc21a4a6bd038d6ee465f51
herishiM <65328061+herishiM@users.noreply.github.com>
2020-06-06 20:42:06 +0530

    Added allBoardData

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -9,27 +9,28 @@ var path = require('path');
 
 const auth = require('../auth');
 
+const UserBoards = require('../models/userboard.model');
 const Boards = require('../models/board.model');
 router.post("/addboard", auth, async (req, res) => {
     console.log("req rcvd");
     let username = req.user.id;
-  //TODO: Check if username has access to edit the file
+    //TODO: Check if username has access to edit the file
     let creator = req.body.creator;
     let boardname = req.body.boardname;
     let board_list = req.body.board_list;
     let users = req.body.users;
     let color = req.body.boardcolor;
     try {
-        let doc = await Boards.findOneAndUpdate({ boardname: boardname},{ boardcolor:color, creator:creator , usernames: users, list: board_list}, {
+        let doc = await Boards.findOneAndUpdate({ boardname: boardname }, { boardcolor: color, creator: creator, usernames: users, list: board_list }, {
             new: true,
             upsert: true // Make this update into an upsert
         });
-        res.status(200).json({doc});
-    } catch(err) {
+        res.status(200).json({ doc });
+    } catch (err) {
         console.log(err);
-        res.status(400).json({err});
+        res.status(400).json({ err });
     }
-    
+
     //Check if board object exists
     /*
     const Boards = await Boards.find({username: username, boardname: boardname}).exec();
@@ -53,19 +54,19 @@ router.post("/getboarddata", auth, async (req, res) => {
     let username = req.user.id;
     //Check if username has access to edit the file
     let creator = req.body.creator;
-    if(creator == '') {
+    if (creator == '') {
         creator = username;
     }
     let boardname = req.body.boardname;
     console.log(boardname);
     console.log(creator);
-    await Boards.findOne({ boardname: boardname}, function(err, board) {
-        if(err) {
+    await Boards.findOne({ boardname: boardname }, function (err, board) {
+        if (err) {
             console.log(err);
-            res.status(400).json({err});
+            res.status(400).json({ err });
             return;
-        } else if(!board) {
-            res.status(400).json({err: "Incorrect board name"});
+        } else if (!board) {
+            res.status(400).json({ err: "Incorrect board name" });
             return;
         } else {
             console.log("board info sent");
@@ -73,6 +74,43 @@ router.post("/getboarddata", auth, async (req, res) => {
             return;
         }
     });
+})
+
+router.post("/allboarddata", auth, async (req, res) => {
+    let username = req.user.id;
+    //Check if username has access to edit the file
+    const userBoards = await UserBoards.find({ username: username }).exec();
+    let board_list = userBoards[0].personalBoards;
+    var allBoardData = [];
+    var flag = true;
+    var len = board_list.length;
+    await board_list.forEach((boardname, index) =>
+        {   
+            if(!flag){
+                return;
+            }
+            console.log("alldata llop");
+            Boards.findOne({ boardname: boardname }, function (err, board) {
+            if (err) {
+                 console.log(err);
+                res.status(400).json({ err });
+                flag = false;
+                return;
+            } else if (!board) {
+                return;
+            } else {
+                console.log("board info sent");
+                allBoardData.push(board);
+                console.log("boardname:" + boardname);
+                console.log(board);
+            }
+
+            if(len - 1 == index) {
+                console.log("allBoardData:" + allBoardData);
+                res.status(200).json(allBoardData);
+            }
+        })}
+    )
 })
 
 module.exports = router;
