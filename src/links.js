edd3064d8613a90095148ab2c4c89661a7c5b488
Pranav <pranav2000joglekar@gmail.com>
2020-06-08 04:25:51 +0530

    Make changes for deploying

--- src/links.js
+++ src/links.js
@@ -1,3 +1,4 @@
-let url = "http://localhost:5000/api/";
+//let url = "http://localhost:5000/api/";
+let url = "/api/";
 //let url = "http://ec2-13-233-224-64.ap-south-1.compute.amazonaws.com/api/api";
 export default url;
